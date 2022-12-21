a0f22b9486a86cdff65cb8c7b8a84125dff517a5
oyedamola abioye <you@example.com>
2022-12-20 18:17:03 -0800

    my first array

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * update pointer value to 98
+ * @n: parameter to be provided
+ * Return:  0 for  (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
